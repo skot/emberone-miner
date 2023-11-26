638e80662cc7b8edbb1f4392ed8c83ceae196fb2
shufps <shufps80@gmail.com>
2023-11-26 18:47:12 +0100

    working fine now

--- piaxe/miner.py
+++ piaxe/miner.py
@@ -4,6 +4,7 @@ import time
 import logging
 from collections import deque
 import random
+import copy
 
 from rpi_hardware_pwm import HardwarePWM
 
@@ -48,11 +49,20 @@ class BM1366Miner:
         self._write_index = 0
         self._buffer = bytearray([0] * 64)
 
+        self._internal_id = 0
+        self._latest_work_id = 0
+        self._jobs = dict()
+        self._timestamp_last_chipid = 0
+
+        self.tracker_send = list()
+        self.tracker_received = list()
 
         self.job_thread = None
+        self.receive_thread = None
         self.job_lock = threading.Lock()
         self.serial_lock = threading.Lock()
         self.stop_event = threading.Event()
+        self.new_job_event = threading.Event()
 
 
     def init(self):
@@ -94,6 +104,13 @@ class BM1366Miner:
             raise Exception("bm1366 not detected")
 
         self.set_difficulty(512)
+
+        self.receive_thread = threading.Thread(target=self._receive_thread)
+        self.receive_thread.start()
+
+        self.job_thread = threading.Thread(target=self._job_thread)
+        self.job_thread.start()
+
 
     def _serial_tx_func(self, data, debug=False):
         with self.serial_lock:
@@ -142,18 +159,21 @@ class BM1366Miner:
     def set_submit_callback(self, cb):
         self.submit_cb = cb
 
-    def _receive(self):
+    def _receive_thread(self):
+        logging.info('receiving thread started ...')
+        #last_response = time.time()
         while True:
             if self.stop_event.is_set():
                 return
 
-            byte = self._serial_rx_func(1, 100, debug=False)
+            byte = self._serial_rx_func(11, 100, debug=False)
 
             if not byte:
                 continue
 
-            self._buffer[self._write_index % 64] = byte[0]
-            self._write_index += 1
+            for i in range(0, len(byte)):
+                self._buffer[self._write_index % 64] = byte[i]
+                self._write_index += 1
 
             if self._write_index - self._read_index >= 11 and self._buffer[self._read_index % 64] == 0xaa and self._buffer[(self._read_index + 1) % 64] == 0x55:
                 data = bytearray([0] * 11)
@@ -164,57 +184,124 @@ class BM1366Miner:
                 logging.debug("<- %s", bytes(data).hex())
 
                 asic_result = bm1366.AsicResult().from_bytes(bytes(data))
-                if asic_result and asic_result.nonce and asic_result.nonce not in [0x0, 0x6613]:
-                    with self.job_lock:
-                        result = dict(
-                            job_id = self.current_job._job_id,
-                            extranonce2 = self.current_job._extranonce2,
-                            ntime = self.current_job._ntime,
-                            nonce = shared.int_to_hex32(asic_result.nonce),
-                            version = shared.int_to_hex32(bm1366.reverse_uint16(asic_result.version) << 13),
-                        )
-                        if not shared.verify_work(self._difficulty, self.current_job, result):
-                            logging.error("invalid result!")
+                if not asic_result or not asic_result.nonce:
+                    continue
 
+                if asic_result.nonce == 0x6613:
+                    self._timestamp_last_chipid = time.time()
+                    continue
+
+                with self.job_lock:
+                    last_response = time.time()
+                    result_job_id = asic_result.job_id & 0xf8
+                    logging.debug("work received %02x", result_job_id)
+
+                    #if result_job_id != self._latest_work_id:
+                    #    logging.warn("discarding result ... too old")
+                    if result_job_id not in self._jobs:
+                        logging.error("internal jobid %d not found", result_job_id)
+                        continue
+
+                    saved_job = self._jobs[result_job_id]
+                    job = saved_job['job']
+                    work = saved_job['work']
+                    difficulty = saved_job['difficulty']
+
+                    if result_job_id != work.id:
+                        logging.error("mismatch ids")
+                        continue
+
+                    result = dict(
+                        job_id = job._job_id,
+                        extranonce2 = job._extranonce2, #shared.int_to_hex32(job._extranonce2),
+                        ntime = job._ntime,
+                        nonce = shared.int_to_hex32(asic_result.nonce),
+                        version = shared.int_to_hex32(bm1366.reverse_uint16(asic_result.version) << 13),
+                    )
+                    is_valid = shared.verify_work(difficulty, job, result)
+
+                    if not is_valid:
+                        logging.error("invalid result!")
+                    else:
                         self.submit_cb(result)
 
-                    # restart miner with new extranonce2
-                    self._start_with_random_extranonce2(self.current_job)
+                # restart miner with new extranonce2
+
+
+    def _job_thread(self):
+        logging.info("job thread started ...")
+        current_time = time.time()
+        while True:
+            # did we receive a new job or are we mining for longer than 10s
+            if not self.new_job_event.is_set() and time.time() - current_time < 10:
+                time.sleep(0.1)
+                continue
+
+            # clear event asap
+            self.new_job_event.clear()
+
+            with self.job_lock:
+                if not self.current_job:
+                    logging.info("no job ...")
+                    time.sleep(1)
+                    continue
+
+
+                extranonce2 = random.randint(0, 2**31-1)
+                logging.debug("new extranonce2 %08x", extranonce2)
+                self.current_job.set_extranonce2(extranonce2)
+
+                self._internal_id += 1
+                self._latest_work_id = ((self._internal_id << 3) & 0x7f) + 0x10
+
+                work = bm1366.WorkRequest()
+                logging.debug("new work %02x", self._latest_work_id)
+                work.create_work(
+                    self._latest_work_id,
+                    0x00000000,
+                    shared.hex_to_int(self.current_job._nbits),
+                    shared.hex_to_int(self.current_job._ntime),
+                    shared.reverse_bytes(shared.hex_to_bytes(self.current_job._merkle_root)),
+                    shared.reverse_bytes(shared.hex_to_bytes(self.current_job._prevhash)),
+                    shared.hex_to_int(self.current_job._version)
+                )
+                self.current_work = work
+
+                # make deepcopies
+                self._jobs[self._latest_work_id] = {
+                    'job': copy.deepcopy(self.current_job),
+                    'work': copy.deepcopy(self.current_work),
+                    'difficulty': self._difficulty
+                }
+
+                # do it every now and then ...
+                bm1366.request_chip_id()
+
+                # logging.info("health-checking ...")
+                # while True:
+                #     bm1366.request_chip_id()
+                #     time.sleep(1)
+                #     if time.time() - self._timestamp_last_chipid < 2000:
+                #         break
+                #     logging.info("health-checking retry ...")
+
+                # logging.info("health-checking success")
+
+                bm1366.send_work(work)
+
+                # remember when we started the work
+                current_time = time.time()
 
 
 
-    def _start_with_random_extranonce2(self, job):
-        with self.job_lock:
-            extranonce2 = random.randint(0, 2**31-1)
-            logging.debug("new extranonce2 %08x", extranonce2)
-            job.set_extranonce2(extranonce2)
 
-            self.current_job = job
-
-            work = bm1366.WorkRequest()
-            work.create_work(
-                shared.hex_to_int(job._job_id),
-                0x00000000,
-                shared.hex_to_int(job._nbits),
-                shared.hex_to_int(job._ntime),
-                shared.reverse_bytes(shared.hex_to_bytes(job._merkle_root)),
-                shared.reverse_bytes(shared.hex_to_bytes(job._prevhash)),
-                shared.hex_to_int(job._version)
-            )
-            self.current_work = work
-            bm1366.send_work(work)
 
 
     def start_job(self, job):
-        self.stop_event.set()
-        if self.job_thread:
-            self.job_thread.join()
-
-        self.stop_event.clear()
-        self.job_thread = threading.Thread(target=self._receive)
-        self.job_thread.start()
-
-        self._start_with_random_extranonce2(job)
+        logging.info("starting new job %s", job._job_id)
+        with self.job_lock:
+            self.current_job = job
+            self.new_job_event.set()
 
 
     def stop(self):
