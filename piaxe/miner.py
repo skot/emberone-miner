c8b48b3eb03e9fcac34dbe6fd7f6904dbc4f76c3
shufps <shufps80@gmail.com>
2023-12-23 18:21:56 +0100

    moved code

--- piaxe/miner.py
+++ piaxe/miner.py
@@ -446,15 +446,15 @@ class BM1366Miner:
                     is_valid, hash, zeros = shared.verify_work(difficulty, job, result)
                     network_target, network_zeros = shared.nbits_to_target(job._nbits)
 
+                    logging.debug("network-target: %s (%d)", network_target, network_zeros)
+                    logging.debug("found hash:     %s (%d)", hash, zeros)
+
                     if is_valid:
                         mask_nonce |= asic_result.nonce
                         mask_version |= asic_result.version << 13
 
                         logging.debug(f"mask_nonce:   %s (%08x)", shared.int_to_bin32(mask_nonce, 4), mask_nonce)
                         logging.debug(f"mask_version: %s (%08x)", shared.int_to_bin32(mask_version, 4), mask_version)
-
-                        logging.debug("network-target: %s (%d)", network_target, network_zeros)
-                        logging.debug("found hash:     %s (%d)", hash, zeros)
 
                     if INFLUX_ENABLED:
                         with self.influx.stats.lock:
