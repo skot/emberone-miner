cece2581188825f2fa535be274f7326dd777f037
shufps <shufps80@gmail.com>
2024-08-24 21:47:51 +0200

    influx stats via rest api /influx/stats

--- piaxe/rest.py
+++ piaxe/rest.py
@@ -6,20 +6,22 @@ import threading
 import logging
 import random
 
-class ASICFrequencyManager:
-    def __init__(self, config, miner):
+class RestAPI:
+    def __init__(self, config, miner, stats):
         self.miner = miner
         self.hardware = miner.hardware
         self.asics = miner.asics
         self.app = Flask(__name__)
         self.cm = self.asics.clock_manager
         self.config = config
+        self.stats = stats
 
         # Define routes
         self.app.add_url_rule('/clocks', 'get', self.get_clocks, methods=['GET'])
         self.app.add_url_rule('/clock/<int:id>', 'set', self.set_clock, methods=['POST'])
         self.app.add_url_rule('/stats', 'get_stats', self.get_stats, methods=['GET'])
         self.app.add_url_rule('/pwm/<int:id>/set', 'set_pwm', self.set_pwm, methods=['POST'])  # Updated to accept variable PWM ID
+        self.app.add_url_rule('/influx/stats', 'get_influx_stats', self.get_influx_stats, methods=['GET'])
 
 
         # Route to serve the index.html
@@ -65,6 +67,35 @@ class ASICFrequencyManager:
                 return jsonify({"error": f"Error setting PWM value for channel {id}"}), 400
         else:
             return jsonify({"error": f"Invalid PWM value for channel {id}. Must be between 0.0 and 1.0"}), 400
+
+    # InfluxDB Stats Endpoint
+    def get_influx_stats(self):
+        with self.stats.lock:
+            stats = {
+                "temperature": self.stats.temp,
+                "temperature2": self.stats.temp2,
+                "temperature3": self.stats.temp3,
+                "temperature4": self.stats.temp4,
+                "vdomain1": self.stats.vdomain1,
+                "vdomain2": self.stats.vdomain2,
+                "vdomain3": self.stats.vdomain3,
+                "vdomain4": self.stats.vdomain4,
+                "hashing_speed": self.stats.hashing_speed,
+                "invalid_shares": self.stats.invalid_shares,
+                "valid_shares": self.stats.valid_shares,
+                "uptime": self.stats.uptime,
+                "best_difficulty": self.stats.best_difficulty,
+                "total_best_difficulty": self.stats.total_best_difficulty,
+                "pool_errors": self.stats.pool_errors,
+                "accepted": self.stats.accepted,
+                "not_accepted": self.stats.not_accepted,
+                "total_uptime": self.stats.total_uptime,
+                "total_blocks_found": self.stats.total_blocks_found,
+                "blocks_found": self.stats.blocks_found,
+                "difficulty": self.stats.difficulty,
+                "duplicate_hashes": self.stats.duplicate_hashes
+            }
+        return jsonify(stats)
 
     def run(self):
         host = self.config.get("host", "127.0.0.1")
